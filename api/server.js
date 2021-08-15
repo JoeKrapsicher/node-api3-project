cc917f04f2d9c2bf2c0079825afcb4c8635f7df6
JoeKrapsicher <JoeKrapsicher@gmail.com>
2021-08-15 12:24:43 -0600

    Completed Assignment

--- api/server.js
+++ api/server.js
@@ -3,11 +3,20 @@ const express = require('express');
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-
+server.use(express.json());
 // global middlewares and the user's router need to be connected here
+const userRouter = require('./users/users-router')
+const middleware = require('./middleware/middleware')
+
+server.use('/api/users', userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 module.exports = server;
+
+
+
+
+//Finish writing middleware, and UserRouters
